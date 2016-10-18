d623330f4d2a8753a8e1843c426d7afe180ab31b
Mateusz Wieczorek <wskmateusz@gmail.com>
2016-10-18 11:48:12 +0200

    Poprawa struktury projektu

---
+++ lab2/src/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by mawieczo on 10/18/16.
+ */
+public class Main {
+
+    private static BufferedReader reader;
+
+    public static void main(String [ ] args) throws IOException {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Wprowadz nazwe pliku");
+        String fileName = reader.readLine();
+        System.out.println("Wprowadź poprawny string");
+        String validString = reader.readLine();
+        System.out.println("Wprowadź niepoprawny string");
+        String invalidString = reader.readLine();
+    }
+}
